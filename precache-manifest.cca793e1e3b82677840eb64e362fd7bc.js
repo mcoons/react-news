1110c6c5aad21c2a421d1bb7d82dcb84bc99eea6
mcoons <mcoons67@gmail.com>
2019-01-04 09:47:17 -0700

    Updates

---
+++ precache-manifest.cca793e1e3b82677840eb64e362fd7bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0c0a51b2bf4389d45df",
+    "url": "/mcoons/react-news/static/js/runtime~main.e0c0a51b.js"
+  },
+  {
+    "revision": "4c9bb8bca093e2105df9",
+    "url": "/mcoons/react-news/static/js/main.4c9bb8bc.chunk.js"
+  },
+  {
+    "revision": "ec7c3b054db072497e7b",
+    "url": "/mcoons/react-news/static/js/1.ec7c3b05.chunk.js"
+  },
+  {
+    "revision": "4c9bb8bca093e2105df9",
+    "url": "/mcoons/react-news/static/css/main.b6b29236.chunk.css"
+  },
+  {
+    "revision": "36d1bd7add4392cd4a3f0cd127e31c42",
+    "url": "/mcoons/react-news/index.html"
+  }
+];
